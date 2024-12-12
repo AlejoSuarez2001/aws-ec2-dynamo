694bfb70c5b78f7ce510fc0ab943f67729471ba7
AlejoSuarez2001 <alejo.2001.suarez@gmail.com>
2024-12-12 13:55:50 -0300

    fix

--- backend/repositories/taskRepository.js
+++ backend/repositories/taskRepository.js
@@ -3,11 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const getTask = async (id) => {
-    console.log("Fetching task");
+    console.log("Fetching one task");
 };
 
 const getTasks = async () => {
-    console.log("Fetching tasks");
+    console.log("Fetching all tasks");
 };
 
 const addTask = async (task) => {
